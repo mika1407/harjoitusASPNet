b16544f583d47aae84c0c28fd379b90cd7475da5
mika1407 <koneautomaatio@hotmail.com>
2021-08-03 23:30:43 +0300

    tietokanta lisätty ja uusi controlleri

---
+++ LainausjarjestelmaMVC/Controllers/EsineetController.cs
@@ -0,0 +1,21 @@
+﻿using LainausjarjestelmaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LainausjarjestelmaMVC.Controllers
+{
+    public class EsineetController : Controller
+    {
+        // GET: Esineet
+        public ActionResult Index()
+        {
+            LainausDBEntities db = new LainausDBEntities();
+            List<Esineet> model = db.Esineet.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
